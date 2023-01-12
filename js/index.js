3b42003c173a0db6beb89a70096c736d85fb3bad
kriss <delormechrist@gmail.com>
2023-01-12 17:06:33 +0100

    type de données

--- js/index.js
+++ js/index.js
@@ -36,7 +36,9 @@ const title = document.querySelector("h1");
 // document.querySelector('h2').style.color = "green";
 
 const questionContainer = document.querySelector(".click-event");
-const btn1 = document.querySelector("#btn-1");
+// const btn1 = document.querySelector("#btn-1");
+const btn1 = document.getElementById("btn-1");
+
 const btn2 = document.querySelector("#btn-2");
 
 const bonneReponse = document.querySelector(".p1");
@@ -45,17 +47,26 @@ const mauvaiseReponse = document.querySelector(".p2");
 
 btn1.addEventListener("click", () => {
     // console.log("click");
-    bonneReponse.style.visibility = "visible";
-    bonneReponse.style.color = "green";
+    // bonneReponse.style.visibility = "visible";
+    // bonneReponse.style.color = "green";
     // mauvaiseReponse.style.visibility = "hidden";
     // mauvaiseReponse.style.display = "none";
+    // bonneReponse.classList.add("show_response");
+    bonneReponse.classList.add("good_response");
+    btn1.style.background = "green";
+
+
+
 })
 btn2.addEventListener('click', () => {
     // console.log("chat");
-    mauvaiseReponse.style.visibility = "visible";
-    mauvaiseReponse.style.color = "red";
+    // mauvaiseReponse.style.visibility = "visible";
+    // mauvaiseReponse.style.color = "red";
     // bonneReponse.style.visibility = "hidden";
-    bonneReponse.style.display = "none";
+    // bonneReponse.style.display = "none";
+    // mauvaiseReponse.classList.add("show_response");
+    mauvaiseReponse.classList.add("bad_response");
+    btn2.style.background = "red";
 
 
 })
@@ -67,3 +78,43 @@ btn2.addEventListener('click', () => {
 
 // })
 
+// const test = document.getElementsByClassName("box");
+// console.log(test);
+
+
+
+// =============================
+// type de données
+// =============================
+
+
+let unText = "voici un texte ";
+console.log(unText);
+let number = 12;
+number = 65;
+console.log(number);
+let boulean = true;
+let boulean1 = false;
+let array = ["je", "suis", 24, true]//tableau
+console.log(array[2]);
+
+let object = {
+    prenom: "christian",
+    nom: "delorme",
+    address: {
+        ville: "Paris",
+        rue: "danton",
+        num: 5
+    }
+}
+console.log(typeof array); // les arrays sont des objects en JS.
+
+// console.log(object.prenom);
+// console.log(object.address.ville);
+// console.log(object.address);
+
+let fleur; //hundefined
+console.log(fleur);
+
+let arbre = null;
+console.log(arbre);
